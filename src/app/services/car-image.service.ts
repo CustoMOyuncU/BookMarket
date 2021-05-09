f29fc7fa5e85a397de66b8601b5314885846ad36
CustoMOyuncU <kraloyuntv2@gmail.com>
2021-05-09 17:40:57 +0300

    First Commit

--- src/app/services/car-image.service.ts
+++ src/app/services/car-image.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { BookImage } from '../models/bookImage';
+import { ListResponseModel } from '../models/listResponseModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarImageService {
+  apiUrl=environment.baseUrl+"/carimages/"
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
+
+  getImagesByBookId(id: number): Observable<ListResponseModel<BookImage>> {
+    return this.httpClient.get<ListResponseModel<BookImage>>(
+      `${this.apiUrl}getcarimagebycarid?id=${id}`
+    );
+  }
+  getBookImageUrl(id: number): string {
+    return `${this.apiUrl}getfilebyid?id=${id}`;
+  }
 }
